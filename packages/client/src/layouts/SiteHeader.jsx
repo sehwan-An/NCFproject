4f2064b89b8941d76370502bf4ba571651ff8212
302-09\HANUL <sehwan4104@gmail.com>
2025-04-10 11:10:26 +0900

    기본구성

---
+++ packages/client/src/layouts/SiteHeader.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+const SiteHeader = () => {
+  return (
+    <div>
+<Container>
+<Row>
+
+
+
+    
+</Row>
+
+
+
+</Container>
+
+    </div>
+  )
+}
+
+export default SiteHeader
--- packages/client/src/routes/Home.jsx
+++ packages/client/src/routes/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <div>
 <SiteHeader/>
-
 <SiteFooter/>
     </div>
   )
