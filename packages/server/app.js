ebca8144ced11a734d1266b7396c2ba56db8d2de
STMT <elahep113@naver.com>
2025-04-10 14:31:40 +0900

    server/app.js | cors 패키지 임포트

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 const app = express();
+import cors from 'cors';
 
 
 import indexRouter from './routes/index.route.js'
 
 
 app.use('/', indexRouter)
+app.use(cors({
+    Credential: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
   
