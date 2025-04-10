2e4902e5cf6d5e90324aa46ca6e566be0056e828
STMT <elahep113@naver.com>
2025-04-10 12:53:44 +0900

    app.jsx | 홈과 signup 중첩 라우팅

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-// import {Routes, Route} from 'react-router';
+import React from 'react';
+import { Routes, Route } from 'react-router';
+import Home from './routes/Home.jsx';
+import SignUp from './routes/SignUp.jsx';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="signup" element={<SignUp />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
