9ffaa7297f35c3ae7966c05ae88e77d4ce73fbbd
302-09\HANUL <sehwan4104@gmail.com>
2025-04-10 16:56:30 +0900

    상품보기, 고객문의 작업준비

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router';
 import Home from './routes/Home.jsx';
 import SignUp from './routes/SignUp.jsx';
+import Shop from './routes/Shop.jsx'
+import contact from './routes/Contact.jsx';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />}>
           <Route path="signup" element={<SignUp />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
