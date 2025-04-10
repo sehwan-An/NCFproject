9bafd6ffbe2f9d5c41b905499bf9993aaa7abf86
STMT <elahep113@naver.com>
2025-04-10 12:11:53 +0900

    outlet 추가

--- packages/client/src/routes/Home.jsx
+++ packages/client/src/routes/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router'
 import SiteHeader from '../layouts/SiteHeader'
 import SiteFooter from '../layouts/SiteFooter'
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
 <SiteHeader/>
+<Outlet></Outlet>
 <SiteFooter/>
     </div>
   )
