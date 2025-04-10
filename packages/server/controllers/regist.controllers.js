5bd9e86de72484d75c1697ba212660d3248ecd06
302-09\HANUL <sehwan4104@gmail.com>
2025-04-10 14:47:02 +0900

    유저 정보 전송

---
+++ packages/server/controllers/regist.controllers.js
@@ -0,0 +1,30 @@
+import User from '../models/User.js';
+
+const regist = async (req, res) => {
+  const { username, userpwd, userid, userphone } = req.body;
+  console.log(req.body);
+
+//   res.send('new user is created!!!!');
+
+  try {
+    const existUser = await User.findOne({
+      $or: [ { username }, { userphone }],
+    });
+    if (existUser) {
+      return res.status(400).json({
+        message: '해당 정보로 등록된 사용자가 이미 존재합니다.',
+      });
+    }
+
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({
+        status: 'success',
+        message:'유저 생성 성공',
+        data: user
+    })
+  } catch (err) {
+    console.log('regist is failed :'.err);
+  }
+};
+export default regist;
