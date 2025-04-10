35b4c19d1d6a88a27e8007e6e4d5ac68ac36967c
STMT <elahep113@naver.com>
2025-04-10 12:54:02 +0900

    footer 공용 ui

--- packages/client/src/layouts/SiteFooter.jsx
+++ packages/client/src/layouts/SiteFooter.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import footerLogo from '../assets/footer-logo.png';
+import { Container } from 'react-bootstrap';
+import '../App.css';
+import { Link } from 'react-router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const SiteFooter = () => {
   return (
-    <div>SiteFooter</div>
-  )
-}
+    <>
+      <Container className="d-flex align-items-center justify-content-around my-3">
+        <div>
+          <img src={footerLogo} alt="footer-logo" />
+        </div>
+        <p className="fs-5">New Casual Fation&copy; has all rights deserved.</p>
+        <div className="d-flex gap-3">
+          <Link>
+            <i className="bi bi-twitter"></i>
+          </Link>
+          <Link>
+            <i className="bi bi-facebook"></i>
+          </Link>
+          <Link>
+            <i className="bi bi-instagram"></i>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-export default SiteFooter
+export default SiteFooter;
