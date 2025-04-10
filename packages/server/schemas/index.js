bec5dc7884e1705c3cb385a245dbd9dfabf81335
302-09\HANUL <sehwan4104@gmail.com>
2025-04-10 14:47:36 +0900

    몽고DB 연결 구성

---
+++ packages/server/schemas/index.js
@@ -0,0 +1,23 @@
+// const mongoose = require('mongoose');
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+const connect = () => {
+  if (process.env.NODE_ENV !== 'production') { 
+    mongoose.set('debug', true)
+  }
+  mongoose.connect(process.env.DB_URI,{
+    authSource:"admin"
+  }).then(() => console.log("Mongodb is connected!"))
+  .catch((err) => console.log(err))
+}
+
+mongoose.connection.on('error', () => {
+    console.log('Mongodb Connect is failed!');
+  });
+//   mongoose.connection.on('disconnected', () => {
+//     console.log('Connect is disconnected!');
+//     connect(); // 연결 재시도
+//   })
+  
+export default connect;
