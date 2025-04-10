828ecef2a34e5f4f999ce3eff278b4efa3cbe86b
302-09\HANUL <sehwan4104@gmail.com>
2025-04-10 10:59:01 +0900

    메인화면 작업준비

---
+++ packages/client/src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SiteHeader from '../layouts/SiteHeader'
+import SiteFooter from '../layouts/SiteFooter'
+
+const Home = () => {
+  return (
+    <div>
+<SiteHeader/>
+
+<SiteFooter/>
+    </div>
+  )
+}
+
+export default Home
