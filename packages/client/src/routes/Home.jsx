dfcd4495dd57e6324190c5b7fcc7e177b9f2a1e9
STMT <elahep113@naver.com>
2025-04-11 12:02:43 +0900

    쿠키 받아서 쿠키에 따른 화면 렌더링

--- packages/client/src/routes/Home.jsx
+++ packages/client/src/routes/Home.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-import SiteHeader from '../layouts/SiteHeader'
-import SiteFooter from '../layouts/SiteFooter'
+import React, { useEffect, useState } from 'react';
+import { Outlet } from 'react-router';
+import SiteHeader from '../layouts/SiteHeader';
+import SiteFooter from '../layouts/SiteFooter';
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 
 const Home = () => {
+  const [userInfo, setUserInfo] = useState(null);
+  useEffect(() => {
+    const token = Cookies.get('NCF');
+    try {
+      if(token) {
+        const decodedToken = jwtDecode(token);
+        setUserInfo(decodedToken)
+        console.log(decodedToken)
+      }
+    } catch(e) {
+      console.log(e)
+    }
+  }, [])
+  const Logout = () => {
+    if(confirm('로그아웃 하시겠습니까?')) {
+      Cookies.remove('NCF');
+      location.href = '/'
+    }
+  }
   return (
     <div>
-<SiteHeader/>
-<Outlet></Outlet>
-<SiteFooter/>
+      <SiteHeader userInfo={userInfo} Logout={Logout} />
+      <Outlet></Outlet>
+      <SiteFooter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
