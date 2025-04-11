450a468b94662b9ff3dc3bee80fd69efa2e6d457
302-09\HANUL <sehwan4104@gmail.com>
2025-04-11 15:37:50 +0900

    bcrypt 작업 준비

--- packages/server/controllers/regist.controllers.js
+++ packages/server/controllers/regist.controllers.js
@@ -1,7 +1,7 @@
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 import 'dotenv/config'
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
 
 const regist = async (req, res) => {
   const { username, userpwd, userid, userphone, email } = req.body;
@@ -19,7 +19,7 @@ const regist = async (req, res) => {
 
     const user = new User({
       username,
-      userpwd: bcrypt.hash(req.body.password),
+      userpwd,
       userid,
       userphone,
       email,
