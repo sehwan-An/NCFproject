9979afd2003f592be2ea83be0efd808d45e143dc
302-09\HANUL <sehwan4104@gmail.com>
2025-04-11 14:27:32 +0900

    post.route.js -> product.route.js 이름 수정

--- packages/server/routes/post.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import controllers from '../controllers/regist.controllers.js';
-const router = express.Router();
-
-
-router.post('/', controllers.regist);
-
-export default router;
