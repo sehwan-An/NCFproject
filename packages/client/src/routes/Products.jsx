244628e66a92f4aeed3ab2b9caeeb5660e55a340
STMT <elahep113@naver.com>
2025-04-11 14:18:46 +0900

    제품 이미지 샘플

--- packages/client/src/routes/Products.jsx
+++ packages/client/src/routes/Products.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Products = () => {
-  const isProduct = console.log('재고 있음');
+  const [products, setProducts] = useState({});
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+    .then((res) => {
+      console.log(res)
+      setProducts(res);
+    })
+    console.log(products)
+
+  }, [products])
+  const isProduct = products.length > 0
   return (
     <>
       <h2 className="text-center">상품관리</h2>
       <div className="text-center my-2">
-        {isProduct ? '재고있음' : '상품 재고 없음'}
+        {isProduct ? (
+          products.map((product, i) => {
+            <>
+              
+            </>
+          })
+        ) : ('상품 재고 없음')}
       </div>
       <div className="text-center">
         <Link to="/addproduct">
