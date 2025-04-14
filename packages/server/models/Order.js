24f55a0b1cb48fb152a8a1d43d42031c523a6d5b
STMT <elahep113@naver.com>
2025-04-14 11:15:43 +0900

    order model | 주문 모델 기초작업

--- packages/server/models/Order.js
+++ packages/server/models/Order.js
@@ -1,35 +1,40 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const orderSchema = mongoose.Schema(
-    {
-        orderuser: {
-            type: String,
-            ref: 'User',
-            required: true
-        },
-        orderphone: {
-            type: String,
-            required: true
-        },
-        destination: {
-            type: String,
-            required: true
-        },
-        orderproduct: {
-            type: String,
-            ref: 'Product',
-            required: true
-        },
-        orderquantity: {
-            type: String,
-            required: true
-        },
-        orderprice: {
-            type: String,
-            required: true
-        },
-        ordercolor: {
-            type: String
-        }
-    }
-)
+  {
+    orderuser: {
+      type: String,
+      ref: 'User',
+      required: true,
+    },
+    orderphone: {
+      type: String,
+      required: true,
+    },
+    destination: {
+      type: String,
+      required: true,
+    },
+    orderproduct: {
+      type: String,
+      ref: 'Product',
+      required: true,
+    },
+    orderquantity: {
+      type: String,
+      required: true,
+    },
+    orderprice: {
+      type: String,
+      required: true,
+    },
+    ordercolor: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
