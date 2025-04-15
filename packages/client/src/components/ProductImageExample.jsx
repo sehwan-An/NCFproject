4dddb6d4a1ee3bc497544c83238a1c25d6f39738
STMT <elahep113@naver.com>
2025-04-15 14:16:09 +0900

    임시 이미지 수정

--- packages/client/src/components/ProductImageExample.jsx
+++ packages/client/src/components/ProductImageExample.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ProductImageExample = ({index, text, photo}) => {
+const ProductImageExample = ({text, photo}) => {
   return (
     <img src={`http://localhost:3000/${photo}`} alt={`${text}`} />
   )
