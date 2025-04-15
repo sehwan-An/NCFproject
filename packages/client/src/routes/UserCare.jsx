d84fc233807a2b1b8bca9d3437e800a6042a81b5
302-09\HANUL <sehwan4104@gmail.com>
2025-04-15 14:55:30 +0900

    관리자 고객관리 작업중
    작성자 및 작성일 추가예정

--- packages/client/src/routes/UserCare.jsx
+++ packages/client/src/routes/UserCare.jsx
@@ -1,4 +1,4 @@
-import { usetState } from 'react'
+import { useEffect, useState } from 'react'
 import React from 'react'
 import axios from 'axios'
 import Cookies from 'js-cookie'
@@ -6,17 +6,39 @@ import {Container, Row, Col} from 'react-bootstrap'
 
 
 const UserCare = () =>  {
+const [posts, setPosts] = useState(null)
 
+
+useEffect(() => {
+const  token = Cookies.get('NCF')
+if(!token) {
+    alert("환영합니다.")
+}
+axios.get('http://localhost:3000/users/contact/',{
+    withCredentials:true,
+})
+.then((res) => {
+    console.log(res)
+    setPosts(res.data)
+})
+.catch((err) => console.log(err.message))
+}, [])
+function convertDate(date){ 
+    return new Date(date).toLocaleDateString('ko-KR', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+  }).replaceAll(',','').replaceAll(' ','-')
+  }
 
 
 
   return (
     <>
-    <Container>
+    <Container className='py-3'>
         <Row>
-            <Col>
-            <h2>고객 문의사항</h2>
-            <table>
+            <h2 className='text-center mb-5'>고객 문의사항</h2>
+            <table className='table'>
                 <thead>
                     <tr>
                         <th>제목</th>
@@ -26,15 +48,17 @@ const UserCare = () =>  {
                     </tr>
                 </thead>
                 <tbody>
+                   {posts && 
+                   posts.map((post)=>(
                     <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <td>{post.contact_title}</td>
+                        <td>{post.contact_type}</td>
+                        <td>{post.guest.}</td>
+                        <td>{convertDate(post.createdAt)}</td>
                     </tr>
+                   ))}
                 </tbody>
             </table>
-            </Col>
         </Row>
     </Container>
     </>
