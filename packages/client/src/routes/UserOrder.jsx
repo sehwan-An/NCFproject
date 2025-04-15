e0b527bf86282d77bb4e54ce0c6755cbed535a83
STMT <elahep113@naver.com>
2025-04-15 17:21:33 +0900

    주문조회

---
+++ packages/client/src/routes/UserOrder.jsx
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useParams } from 'react-router';
+import ProductImageExample from '../components/ProductImageExample';
+import '../App.css'
+
+const UserOrder = () => {
+  const [orders, setOrders] = useState([]);
+
+  let params = useParams();
+  useEffect(() => {
+    try {
+      axios
+        .get(`http://localhost:3000/api/order/${params.id}`, {
+          withCredentials: true,
+          headers: {
+            userid: `${params.id}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setOrders(res.data);
+        })
+        .catch((e) => console.log(e));
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+  return (
+    <>
+      <Container>
+        <h2 className="text-center">주문내역</h2>
+        <Row>
+          {orders.map((order, i) => {
+            return (
+              <>
+                <Col>
+                  <div>
+                    <ProductImageExample photo={order.photo} text='photo' />
+                  </div>
+                  <div className='order-box'>
+                    <p>주문 고객 : {order.orderuser}</p>
+                    <p>주문번호 : {order.ordernumber}</p>
+                    <p>주문제품 : {order.orderproducts}</p>
+                    <p>주문 수량 : {order.ordercount}</p>
+                    <p>제품 사이즈 : {order.ordersize}</p>
+                    <p>제품가 : {order.orderprice}</p>
+                    <p>색상 : {order.ordercolor}</p>
+                    <p>배송지 : {order.orderaddress}</p>
+                    <p>주문일시 : {order.createdAt.split('T')}</p>
+                  </div>
+                </Col>
+              </>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default UserOrder;
