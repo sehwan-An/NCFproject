c5cfb733987a0598296444710ce3183e2d561fd4
choi <ms10111@nate.com>
2025-04-15 14:54:23 +0900

    shopitem 기초틀 작업

--- packages/client/src/routes/ShopItem.jsx
+++ packages/client/src/routes/ShopItem.jsx
@@ -1,10 +1,154 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Col, Container, Row } from 'react-bootstrap';
 const ShopItem = () => {
   // 이름, 가격, 수량, 이미지, 제품아이디...
   return (
-    <div>ShopItem</div>
+    <div>
+    <container>
+      <img src={`product`} alt="lorem picsum"/>
+      <p>이름</p>
+      <p>가격</p>
+      <p>수량</p>
+      <button
+          type="submit"
+          className="btn btn-primary"
+      >
+          구매하기
+      </button>
+    </container>
+    ShopItem</div>
   )
 }
 
 export default ShopItem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
