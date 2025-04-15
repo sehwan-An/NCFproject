43bdec37a518f81c4c1ec3a70ff3a987fc6117fd
STMT <elahep113@naver.com>
2025-04-15 14:51:35 +0900

    이미지 컴포넌트 삽입

--- packages/client/src/routes/OrderProduct.jsx
+++ packages/client/src/routes/OrderProduct.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useParams, useNavigate } from 'react-router';
 import Cookies from 'js-cookie';
 import { Button, Form, Container, Row, Col, InputGroup } from 'react-bootstrap';
+import ProductImageExample from '../components/ProductImageExample';
 
 const OrderProduct = () => {
   const [validated, setValidated] = useState(false);
@@ -70,83 +71,90 @@ const OrderProduct = () => {
 
   return (
     <>
-      <Container className="w-50">
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="4" controlId="validationCustom01">
-              <Form.Label>제품명</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                disabled
-                name="productname"
-                placeholder="제품명"
-                defaultValue={formData.productname}
-              />
-              <Form.Control.Feedback></Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationCustom02">
-              <Form.Label>제품색상</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                name="productcolor"
-                disabled
-                placeholder="Last name"
-                defaultValue={formData.productcolor}
-              />
-              <Form.Control.Feedback></Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationCustomproductsize">
-              <Form.Label>제품사이즈</Form.Label>
-              <InputGroup hasValidation>
-                <Form.Select
-                  type="text"
-                  placeholder="productsize"
-                  name="productsize"
-                  onChange={handleChange}
-                  required
-                  defaultValue={formData.productsize}
-                >
-                    <option value="S">S</option>
-                    <option value="M">M</option>
-                    <option value="L">L</option>
-                    <option value="XL">XL</option>
-                </Form.Select>
-                <Form.Control.Feedback type="invalid">사이즈를 선택해주세요.</Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="6" controlId="validationCustom04">
-              <Form.Label>주소</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={handleChange}
-                value={formData.addressf}
-                placeholder="주소"
-                name='addressf'
-                required
-              />
-              <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="validationCustom05">
-              <Form.Label>상세주소</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={handleChange}
-                name="addressb"
-                value={formData.addressb}
-                placeholder="상세주소"
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                정확한 주소지를 입력해주세요.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Button type="submit">주문확인</Button>
-        </Form>
+      <Container className="w-100 my-3">
+        <Row className='align-items-center'>
+          <Col className='text-center'>
+            <ProductImageExample photo={formData.photo} text='photo' />
+          </Col>
+          <Col>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="4" controlId="validationCustom01">
+                  <Form.Label>제품명</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    disabled
+                    name="productname"
+                    placeholder="제품명"
+                    defaultValue={formData.productname}
+                  />
+                  <Form.Control.Feedback></Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                  <Form.Label>제품색상</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    name="productcolor"
+                    disabled
+                    placeholder="Last name"
+                    defaultValue={formData.productcolor}
+                  />
+                  <Form.Control.Feedback></Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustomproductsize">
+                  <Form.Label>제품사이즈</Form.Label>
+                  <InputGroup hasValidation>
+                    <Form.Select
+                      type="text"
+                      placeholder="productsize"
+                      name="productsize"
+                      onChange={handleChange}
+                      required
+                      defaultValue={formData.productsize}
+                    >
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">사이즈를 선택해주세요.</Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="6" controlId="validationCustom04">
+                  <Form.Label>주소</Form.Label>
+                  <Form.Control
+                    type="text"
+                    onChange={handleChange}
+                    value={formData.addressf}
+                    placeholder="주소"
+                    name='addressf'
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="6" controlId="validationCustom05">
+                  <Form.Label>상세주소</Form.Label>
+                  <Form.Control
+                    type="text"
+                    onChange={handleChange}
+                    name="addressb"
+                    value={formData.addressb}
+                    placeholder="상세주소"
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    정확한 주소지를 입력해주세요.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Button type="submit">주문확인</Button>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     </>
   );
