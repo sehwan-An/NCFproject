a8843408bec9a20e56506c4dff0a4633c8329204
STMT <elahep113@naver.com>
2025-04-16 12:28:38 +0900

    출력 이미지 사이즈 조정

--- packages/client/src/components/ProductImageExample.jsx
+++ packages/client/src/components/ProductImageExample.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const ProductImageExample = ({text, photo}) => {
   return (
-    <img src={`http://localhost:3000/${photo}`} alt={`${text}`} />
+    <img src={`http://localhost:3000/${photo}`} alt={`${text}`} className='img-size border' />
   )
 }
 
