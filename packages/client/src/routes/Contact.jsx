d666e811e68c58d2bbe779625dda0f52421724f4
302-09\HANUL <sehwan4104@gmail.com>
2025-04-16 16:04:36 +0900

    고객문의사항 페이지 코드 리팩토링 및 히든 폼 추가
    1. default 값으로 미해결

--- packages/client/src/routes/Contact.jsx
+++ packages/client/src/routes/Contact.jsx
@@ -1,50 +1,51 @@
 import React from 'react';
-import {useState} from 'react'
+import { useState } from 'react';
 import axios from 'axios';
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 import { Col, Container, Row, Form, InputGroup } from 'react-bootstrap';
 
-
-
-function Contact  ()  {
-  const [validated , setValidated] = useState(false);
+function Contact() {
+  const [validated, setValidated] = useState(false);
   const [formData, setFormData] = useState({
-    contact_title:'',
-    contact_content:'',
-    contact_type:''
-  })
+    contact_title: '',
+    contact_content: '',
+    contact_type: '',
+  });
 
   const handleChange = (e) => {
     setFormData({
-        ...formData,
-        [e.target.name]: e.target.value
-    })
-}
-  
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.currentTarget;
-    if(form.checkValidity() === false) {
+    if (form.checkValidity() === false) {
       event.stopPropagation();
     }
     setValidated(true);
-    const token = Cookies.get('NCF')
-    if(!token) {
-      alert("로그인후 이용해주세요!")
-      location.href='/signin'
+    const token = Cookies.get('NCF');
+    if (!token) {
+      alert('로그인후 이용해주세요!');
+      location.href = '/signin';
     }
-    axios.post('http://localhost:3000/users/contact', formData, {
-      withCredentials: true,
-      headers: {
-        'Content-type':'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    }).then((res) => {
-      if(res.status === 201){
-        location.href = '/'
-      }
-    }).catch((err) => console.error(err.message))
-  }
+    axios
+      .post('http://localhost:3000/users/contact', formData, {
+        withCredentials: true,
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        if (res.status === 201) {
+          location.href = '/';
+        }
+      })
+      .catch((err) => console.error(err.message));
+  };
 
   return (
     <Container>
@@ -52,41 +53,45 @@ function Contact  ()  {
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>제목</Form.Label>
-          <Form.Control type="text"
-          onChange={handleChange}
-          
-          name="contact_title" />
+          <Form.Control type="text" onChange={handleChange} name="contact_title" />
         </Form.Group>
-        
-<Form.Group>
-  <Form.Label>
-    <InputGroup>
-    <Form.Select
-    placeholder="문의유형"
-    required
-    name='contact_type'
-    value={formData.contact_type}
-    onChange={handleChange}
->
-  <option value="">문의유형</option>
-  <option value="제품 문의">제품 문의</option>
-  <option value="반품">반품</option>
-  <option value="교환">교환</option>
-  <option value="환불">환불</option>
-  <option value="기타">기타</option>
-    </Form.Select>
-    </InputGroup>
-  </Form.Label>
-</Form.Group>
+
+        <Form.Group>
+          <Form.Label>
+            <InputGroup>
+              <Form.Select
+                placeholder="문의유형"
+                required
+                name="contact_type"
+                value={formData.contact_type}
+                onChange={handleChange}
+              >
+                <option value="">문의유형</option>
+                <option value="제품 문의">제품 문의</option>
+                <option value="반품">반품</option>
+                <option value="교환">교환</option>
+                <option value="환불">환불</option>
+                <option value="기타">기타</option>
+              </Form.Select>
+            </InputGroup>
+          </Form.Label>
+        </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>내용</Form.Label>
-          <Form.Control as="textarea"  onChange={handleChange} rows={5}
-          name="contact_content" />
+          <Form.Control as="textarea" onChange={handleChange} rows={5} name="contact_content" />
         </Form.Group>
-        <button type='submit'  className='flex-end'>문의하기</button>
+
+        <Form.Group className="mb-3 d-none">
+          <label className="visually-hidden">Hidden input label</label>
+          <input type="text" className="form-control" name="contact_status" id="contact_status" />
+        </Form.Group>
+
+        <button type="submit" className="flex-end">
+          문의하기
+        </button>
       </Form>
     </Container>
   );
-};
+}
 
 export default Contact;
