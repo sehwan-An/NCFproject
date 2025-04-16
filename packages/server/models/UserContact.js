619d1bd10268d4f35710ff49a5a2750efee74a64
302-09\HANUL <sehwan4104@gmail.com>
2025-04-16 16:07:34 +0900

    오류수정
    1.userid 추가

--- packages/server/models/UserContact.js
+++ packages/server/models/UserContact.js
@@ -1,42 +1,49 @@
-import mongoose from "mongoose";
-import { v4 as uuidv4} from 'uuid'
+import mongoose from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
 
 const usercontactSchema = mongoose.Schema(
-    {
-uuid:{
-    type:String,
-    default:uuidv4,
-    unique:true,
-    required:true
-},
-contact_type:{
-    type:String,
-    required:true,
-    enum:['제품 문의','반품','교환','환불','기타']
-},
-contact_title:{
-    type:String,
-    required:true
-},
-
-contact_content:{
-    type:String,
-    required:true
-},
-author:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'User',
-    required:true
-}
-
-
+  {
+    uuid: {
+      type: String,
+      default: uuidv4,
+      unique: true,
+      required: true,
     },
-    {tiemstamps:true}
-)
-usercontactSchema.pre('save', async function(next){
-    next();
-})
+    userid: {
+      type: String,
+      required: true,
+    },
+    contact_type: {
+      type: String,
+      required: true,
+      enum: ['제품 문의', '반품', '교환', '환불', '기타'],
+    },
+    contact_title: {
+      type: String,
+      required: true,
+    },
 
-const Contact = mongoose.model('UserContact', usercontactSchema)
+    contact_content: {
+      type: String,
+      required: true,
+    },
+    contact_status: {
+      type: String,
+      required: true,
+      default: '미해결',
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+usercontactSchema.pre('save', async function (next) {
+  next();
+});
+
+const Contact = mongoose.model('UserContact', usercontactSchema);
 
 export default Contact;
