ec4016800f8cac93ad6e3ef18c5356220cff4826
STMT <elahep113@naver.com>
2025-04-16 12:30:20 +0900

    뒤로가기 버튼 기능 추가 주문 취소 버튼 추가 ( 기능 예정)

--- packages/client/src/routes/UserOrder.jsx
+++ packages/client/src/routes/UserOrder.jsx
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import ProductImageExample from '../components/ProductImageExample';
-import '../App.css'
+import '../App.css';
 
 const UserOrder = () => {
   const [orders, setOrders] = useState([]);
 
+  let navigate = useNavigate();
   let params = useParams();
   useEffect(() => {
     try {
@@ -20,7 +21,7 @@ const UserOrder = () => {
           },
         })
         .then((res) => {
-          console.log(res.data);
+          // console.log(res.data);
           setOrders(res.data);
         })
         .catch((e) => console.log(e));
@@ -28,31 +29,41 @@ const UserOrder = () => {
       console.log(e);
     }
   }, []);
+  const handleNavigate = (e) => {
+    navigate(`/user/${params.id}`);
+  };
+  const handleCancel = (e) => {
+    console.log('주문취소 : ', e.target)
+  }
   return (
     <>
-      <Container>
+      <Container className='my-3'>
         <h2 className="text-center">주문내역</h2>
+        <div className="text-end">
+          <Button onClick={handleNavigate}>뒤로가기</Button>
+        </div>
         <Row>
           {orders.map((order, i) => {
             return (
-              <>
-                <Col key={i}>
-                  <div>
-                    <ProductImageExample photo={order.photo} text='photo' />
-                  </div>
-                  <div className='order-box'>
-                    <p>주문 고객 : {order.orderuser}</p>
-                    <p>주문번호 : {order.ordernumber}</p>
-                    <p>주문제품 : {order.orderproducts}</p>
-                    <p>주문 수량 : {order.ordercount}</p>
-                    <p>제품 사이즈 : {order.ordersize}</p>
-                    <p>제품가 : {order.orderprice}</p>
-                    <p>색상 : {order.ordercolor}</p>
-                    <p>배송지 : {order.orderaddress}</p>
-                    <p>주문일시 : {order.createdAt.split('T')[0]}</p>
-                  </div>
-                </Col>
-              </>
+              <Col key={i} className="border border-success rounded-1 border-opacity-50 p-2 m-2">
+                <div className="text-center">
+                  <ProductImageExample photo={order.photo} text="photo" />
+                </div>
+                <div className="order-box">
+                  <p>주문 고객 : {order.orderuser.username}</p>
+                  <p>주문번호 : {order.ordernumber}</p>
+                  <p>주문제품 : {order.orderproducts}</p>
+                  <p>주문 수량 : {order.ordercount}</p>
+                  <p>제품 사이즈 : {order.ordersize}</p>
+                  <p>제품가 : {order.orderprice}</p>
+                  <p>색상 : {order.ordercolor}</p>
+                  <p>배송지 : {order.orderaddress}</p>
+                  <p>주문일시 : {order.createdAt.split('T')[0]}</p>
+                </div>
+                <div>
+                  <Button onClick={handleCancel}>주문취소</Button>
+                </div>
+              </Col>
             );
           })}
         </Row>
